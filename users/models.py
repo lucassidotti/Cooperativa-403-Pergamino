2c2e721acf0b17dc883453d2d8a6f209b0809c9a
Lucas Sidotti <lucassidotti@gmail.com>
2025-12-25 23:16:18 -0300

    funciones para generar perfiles al cargar cada usuario, empezando a definir los roles dentro del sistema. modelos creados(user,admin)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Perfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    es_alumno = models.BooleanField(default=False)
+    es_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
